4f0de0077bdcf4f0cc9ca81c1d284ee910a41682
DeathMaster001 <austinross173@gmail.com>
2026-08-31 19:47:28 -0400

    Added dark mode toggle

--- script.js
+++ script.js
@@ -144,3 +144,15 @@ cards.forEach(cardImage => {
 
     gameBoard.appendChild(card);
 });
+
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        themeToggle.textContent = "☀️ Light Mode";
+    } else {
+        themeToggle.textContent = "🌙 Dark Mode";
+    }
+});
